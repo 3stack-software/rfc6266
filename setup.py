d31239cb4466790886b1a6046dfc64f527f9552c
Nathan Muir <ndmuir@gmail.com>
2016-07-06 17:06:11 +1000

    Vendorize as `3stack-rfc6266`

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     description='Parse and generate Content-Disposition headers',
     url='https://github.com/g2p/rfc6266',
     keywords='rfc6266 Content-Disposition http attachments',
-    name='rfc6266',
-    version='0.0.4',  # semver
+    name='3stack-rfc6266',
+    version='0.0.5',  # semver
     license='GNU LGPL',
     platforms='OS-independent',
     py_modules=['rfc6266', 'test_rfc6266'],
